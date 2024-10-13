23f8cc26153b1e5dda4a01292dc3459af570cea8
MahmoudAlRefaey <mahmoudmuhammad003@gmail.com>
2024-10-13 13:28:34 +0300

    Cleaned and refactored code in model/main.py and app/main.py

--- app/main.py
+++ app/main.py
@@ -6,67 +6,41 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-def add_predictions(input_data):
-    model = pickle.load(open("model/model.pkl", 'rb'))    
-    scaler = pickle.load(open("model/scaler.pkl", 'rb'))   
-    
-    input_array = np.array(list(input_data.values())).reshape(1,-1)
-    
-    print("Input array before scaling:", input_array)  # Debug print
-    
-    input_array_scaled = scaler.transform(input_array)
-    
-    prediction = model.predict(input_array_scaled)
-    
-    
-    st.subheader('Risk Flag Prediction')
-    
-    if prediction[0] == 0:
-        st.markdown('<p style="color:#38B000;">Eligable </p>', unsafe_allow_html=True)
-    elif prediction[0] == 1:
-        st.markdown('<p style="color:#ff4b4b;">Risk </p>', unsafe_allow_html=True)
-    
-    st.write("Probability of being Eligable: ", model.predict_proba(input_array_scaled)[0][0])
-    st.write("Probability of being Risky: ", model.predict_proba(input_array_scaled)[0][1])
-
 def get_data():
-
-    # Read the data
+    """
+    Read and clean the data.
+    """
     data = pd.read_csv("datasets/Training Data.csv")
-    
-    # Clean the data
     conversion_rate = 0.011909906
     data['Income'] = data['Income'] * conversion_rate
-    
     data = data.drop(['Id', 'CITY', 'STATE', 'CURRENT_HOUSE_YRS'], axis=1)
-    
-    print(data.head())
-    
     return data
 
+
 def add_sidebar():
+    """
+    Create the sidebar with input fields.
+    """
     st.sidebar.header("Loan holder information")
-    
     data = get_data()
     label_encoder = LabelEncoder()
-    
+
     slider_labels = [
-    ("Income ($)", "Income"),
-    ("Age", "Age"),
-    ("Experience (Years)", "Experience"),
-    ("Current Job (Years)", "CURRENT_JOB_YRS"),
+        ("Income ($)", "Income"),
+        ("Age", "Age"),
+        ("Experience (Years)", "Experience"),
+        ("Current Job (Years)", "CURRENT_JOB_YRS"),
     ]
     dropdown_labels = [
         ("Marital Status", "Married/Single"),
         ("House Ownership", "House_Ownership"),
         ("Car Ownership", "Car_Ownership"),
         ("Profession", "Profession"),
-    ]   
-    
-    
+    ]
+
     input_dict = {}
     encoders = {}
-    
+
     for label, key in slider_labels:
         input_dict[key] = st.sidebar.slider(
             label,
@@ -75,53 +49,72 @@ def add_sidebar():
             value=int(data[key].mean())
         )
 
-
     for label, key in dropdown_labels:
-        
-        unique_values = data[key].unique()  # Get the unique options for dropdown
+        unique_values = data[key].unique()
         label_encoder.fit(unique_values)
-        
         encoders[key] = label_encoder
-        
         selected_option = st.sidebar.selectbox(
             label,
             options=unique_values
         )
-        
         encoded_option = label_encoder.transform([selected_option])[0]
         input_dict[key] = encoded_option
 
-
     return input_dict
-    
+
+
+def add_predictions(input_data):
+    """
+    Make predictions using the model.
+    """
+    model = pickle.load(open("model/model.pkl", 'rb'))
+    scaler = pickle.load(open("model/scaler.pkl", 'rb'))
+
+    input_array = np.array(list(input_data.values())).reshape(1, -1)
+    input_array_scaled = scaler.transform(input_array)
+    prediction = model.predict(input_array_scaled)
+
+    st.subheader('Risk Flag Prediction')
+
+    if prediction[0] == 0:
+        st.markdown('<p style="color:#38B000;">Eligable </p>', unsafe_allow_html=True)
+    elif prediction[0] == 1:
+        st.markdown('<p style="color:#ff4b4b;">Risk </p>', unsafe_allow_html=True)
+
+    st.write("Probability of being Eligable: ", model.predict_proba(input_array_scaled)[0][0])
+    st.write("Probability of being Risky: ", model.predict_proba(input_array_scaled)[0][1])
+
 
 def main():
+    """
+    Main function.
+    """
     st.set_page_config(
         page_title="LoanWise",
         page_icon='🏦',
         layout="wide",
         initial_sidebar_state="expanded"
     )
-    
+
     get_data()
     input_data = add_sidebar()
-    
+
     with st.container():
         st.title("Smarter Loan Risk Prediction for Confident Banking Decisions")
         st.write("Unlock the power of smart lending with LoanWise! Our cutting-edge tool Powered by AI  & uses advanced analytics to predict loan eligibility at a glance."
                  "\nGet insights into your financial future and make confident borrowing decisions with ease!")
-        
-    col1, col2 = st.columns([4,1])
-    
+
+    col1, col2 = st.columns([4, 1])
+
     with col1:
         add_predictions(input_data)
-        if input_data['House_Ownership'] == np.int32(0):  # Direct comparison
+        if input_data['House_Ownership'] == np.int32(0):
             st.markdown('<p style="color:#ff4b4b;">Ask about their living situation. </p>', unsafe_allow_html=True)
-        if input_data["CURRENT_JOB_YRS"] > input_data["Experience"] :
+        if input_data["CURRENT_JOB_YRS"] > input_data["Experience"]:
             st.markdown('<p style="color:#ff4b4b;">Ask about their work experience in details. </p>', unsafe_allow_html=True)
 
-    
     st.write("⚠️ This app can aid in predicting load risks, but it should not replace the expertise of a qualified professional.")
+
 
 if __name__ == '__main__':
     main()
--- model/main.py
+++ model/main.py
@@ -1,73 +1,86 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, accuracy_score
 from imblearn.over_sampling import SMOTE
-
 import pickle
 
 
 def get_data_clean():
+    """
+    Load, clean, and preprocess the data.
+    """
+    data = pd.read_csv("datasets/Training Data.csv")
 
-    data  = pd.read_csv("datasets/Training Data.csv")
     # Clean the data
     conversion_rate = 0.011909906
-    data['Income'] = data['Income'] * conversion_rate
+    data['Income'] *= conversion_rate
     data = data.drop(['Id', 'CITY', 'STATE', 'CURRENT_HOUSE_YRS'], axis=1)
-    
-    label_encoder = LabelEncoder()
+
+    # Encode object columns
     object_columns = data.select_dtypes(include=['object']).columns
+    label_encoder = LabelEncoder()
     for column in object_columns:
-        data[column] = label_encoder.fit_transform (data[column])
-    
+        data[column] = label_encoder.fit_transform(data[column])
+
+    # Split data into features and target
+    X = data.drop("Risk_Flag", axis=1)
     y = data["Risk_Flag"]
-    X = data.drop(["Risk_Flag"], axis=1)
-        
+
+    # Perform SMOTE
     smote = SMOTE(random_state=42)
     X_resampled, y_resampled = smote.fit_resample(X, y)
-        
+
+    # Reset the index
     data = pd.DataFrame(X_resampled, columns=X.columns)
     data['Risk_Flag'] = y_resampled
-    
+
     return data
 
+
 def create_model(data):
-    
-    y = data["Risk_Flag"]
-    X = data.drop(["Risk_Flag"], axis=1)
-    
-    # Scale the data
-    scaler = StandardScaler()
-    X[:] = scaler.fit_transform(X[:])
-    
+    """
+    Train and evaluate a decision tree model.
+    """
     # Split the data
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+        data.drop("Risk_Flag", axis=1), data["Risk_Flag"], test_size=0.3, random_state=42
     )
-    
-    # Train the model with Logistic Regression
+
+    # Scale the data
+    scaler = StandardScaler()
+    X_train[:] = scaler.fit_transform(X_train[:])
+    X_test[:] = scaler.transform(X_test[:])
+
+    # Train the model
     model = DecisionTreeClassifier()
-    model.fit(X_train,y_train)
-    
+    model.fit(X_train, y_train)
+
     # Test the model
     y_pred = model.predict(X_test)
-    # Model Evaluation
+
+    # Model evaluation
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"\nAccuracy: {accuracy: .2f}\n")
-    print(f"Classification report: \n {classification_report(y_pred, y_test)}")
+    print(f"\nAccuracy: {accuracy:.2f}\n")
+    print(f"Classification report: \n {classification_report(y_test, y_pred)}")
+
     return model, scaler
-    
+
+
 def main():
+    """
+    Train and save the model.
+    """
     data = get_data_clean()
     model, scaler = create_model(data)
-    
+
     with open('model/model.pkl', 'wb') as f:
         pickle.dump(model, f)
     with open('model/scaler.pkl', 'wb') as f:
         pickle.dump(scaler, f)
-        
+
+
 if __name__ == '__main__':
     main()
